384529132f324e06aac7cbca387baba633a73bdb
Aniket8929 <aniketkumarrai271@gmail.com>
2026-02-06 17:50:07 +0530

    first commit

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,12 +1,12 @@
 import App from "@/App";
 import { AuthLayout } from "@/Auth/Auth.layout";
 import Loginform from "@/Auth/Loginform";
-import Signup from "@/Auth/SignUp";
 import Dashboard from "@/Pages/Dashboard"
 import Herosection from "@/Pages/Herosection"
 
 import { createBrowserRouter } from "react-router-dom"
 import ProtectedRoute from "./ProtectedRoute";
+import Signup from "@/Auth/Signup";
 const Router = createBrowserRouter([
     {
         path: "/",
